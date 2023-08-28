0f0ec0c834b583cbaafe826c3cf61e17265e5db4
Yang <113082249+1234567Yang@users.noreply.github.com>
2023-08-27 18:02:50 -0700

    first submit
    首次提交

---
+++ Quiz/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+
+namespace Quiz
+{
+    public partial class Form1 : Form
+    {
+        public static string quizFile;
+        public static void checkAccess(string file)
+        {
+            Console.WriteLine($"First:{file}");
+            string textFile = file + ".acc";
+            Console.WriteLine($"Last:{textFile}");
+            try
+            {
+
+                
+                File.WriteAllText(textFile, $"Access check passed - UTC: {DateTime.UtcNow}");
+            }
+            catch
+            {
+                Message.dialog.showError("The program has no access to your file, please run as administrator");
+                Environment.Exit(1);
+            }
+        }
+        public static int checkInput() // 0 - empty; 1 - file existed; 2 - directory existed;
+                                 // 3 - directory / file not existed
+                                 // 4 - 胡编乱造（不存在的目录）
+                                 // If no access to the file, the program will close;
+        {
+            if (quizFile == "")
+            {
+                Message.dialog.showHit("Please input a file.");
+                return 0;
+            }
+            else
+            {
+                if (File.Exists(quizFile))
+                {
+                    checkAccess(quizFile);
+                    return 1;
+                }
+                else if (Directory.Exists(quizFile))
+                {
+                    checkAccess(quizFile);
+                    return 2;
+                }
+                else if (quizFile.IndexOf(".") != -1)
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 4;
+                }
+            }
+        }
+        private void CreateForm<TForm>() where TForm : Form//定义一个泛型，然后呢，把它限定在必须继承自Form这个interface
+        {
+            TForm form = Activator.CreateInstance<TForm>(); //Activator来创建一个实例
+
+            form.FormClosed += (sender_, e_) => //拉姆达表达式（看文档 + 盲猜的用法，竟然过编译器了）
+            {
+                this.Show();
+            };
+            form.Icon = Properties.Resources.LOGO;
+            form.Show();
+            this.Hide();
+        }
+
+        private void WriteFile()
+        {
+            //List<string> SoundFile = Directory.GetFiles("C:\\ProgramData\\TakeAQuiz\\","*.wav").ToList();
+            try
+            {
+                if(!Directory.Exists("C:\\ProgramData\\TakeAQuiz\\")) Directory.CreateDirectory("C:\\ProgramData\\TakeAQuiz\\");
+
+                if (!File.Exists("C:\\ProgramData\\TakeAQuiz\\Music1.wav"))
+                {
+                    FileStream fileStream = new FileStream("C:\\ProgramData\\TakeAQuiz\\Music1.wav", FileMode.Create);
+                    byte[] buffer = new byte[Properties.Resources.Music1.Length];
+                    Properties.Resources.Music1.Read(buffer, 0, (int)Properties.Resources.Music1.Length);
+                    fileStream.Write(buffer, 0, buffer.Length);
+                    fileStream.Flush();
+                    Properties.Resources.Music1.Close();
+                    fileStream.Close();
+                }
+                if (!File.Exists("C:\\ProgramData\\TakeAQuiz\\Music2.wav"))
+                {
+                    FileStream fileStream = new FileStream("C:\\ProgramData\\TakeAQuiz\\Music2.wav", FileMode.Create);
+                    byte[] buffer = new byte[Properties.Resources.Music2.Length];
+                    Properties.Resources.Music2.Read(buffer, 0, (int)Properties.Resources.Music2.Length);
+                    fileStream.Write(buffer, 0, buffer.Length);
+                    fileStream.Flush();
+                    Properties.Resources.Music2.Close();
+                    fileStream.Close();
+                }
+
+                //File.WriteAllBytes("C:\\ProgramData\\TakeAQuiz\\Music1.wav", Properties.Resources.Music1);
+
+
+            }
+            catch(Exception ex)
+            {
+                Message.dialog.showError("Can not create and write to the floder:C:\\ProgramData\\TakeAQuiz\\");
+                Message.dialog.showError(ex);
+                Environment.Exit(3);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkInput() == 0)
+            {
+                return;
+            }
+            else if (checkInput() != 1)
+            {
+                Message.dialog.showHit("File does not exist.");
+                //MessageBox.Show("File does not exist!", "Hit:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //CreateForm(Form2);
+            CreateForm<Form2>(); //奇怪的泛型...
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            quizFile = textBox1.Text;
+
+            WriteFile();
+            //this.Icon = Properties.Resources.LOGO; form1删掉了
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            quizFile = textBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (checkInput() == 0)
+            {
+                return;
+            }
+            else if (checkInput() != 1)
+            {
+                //MessageBox.Show("File does not exist!", "Hit:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Message.dialog.showHit("File does not exist!");
+                return;
+            }
+            CreateForm<Form3>();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkInput() == 0)
+            {
+                return;
+            }
+            else if (checkInput() == 4)
+            {
+                Message.dialog.showError("Your file name is wrong!");
+            }
+            else if (checkInput() == 2 || checkInput() == 3)
+            {
+                if (checkInput() == 3)
+                {
+                    //Console.WriteLine(textBox1.Text.Substring(0, textBox1.Text.IndexOf(".")));
+
+                }
+                if (MessageBox.Show("File does not exist, do you want to create it and continue?", "Question:", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(textBox1.Text.Substring(0, textBox1.Text.LastIndexOf("\\")));
+                    }
+                    catch
+                    {
+                        Message.dialog.showError("The program has no access to your file, please run as administrator");
+                        Environment.Exit(1);
+                    }
+
+                    try
+                    {
+                        File.WriteAllText(textBox1.Text, "");
+                    }
+                    catch
+                    {
+                        Message.dialog.showError("The program has no access to your file, please run as administrator");
+                        Environment.Exit(1);
+                    }
+                    
+                }
+                else
+                {
+                    return;
+                }
+
+            }
+
+
+            CreateForm<Form4>();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+
+
+
+    namespace Message
+    {
+        class dialog
+        {
+            public static void Damaged()
+            {
+                MessageBox.Show("This file is not an quiz file or it is damaged!", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            public static void showError(Exception ex)
+            {
+                MessageBox.Show($"An error occured:\n{ex.Message}\n{ex.Data}", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            public static void showError(string text)
+            {
+                MessageBox.Show($"An error occured:\n{text}", "Error:", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            public static void showHit(string text)
+            {
+                MessageBox.Show(text, "Hit:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ Quiz/Form2.cs
@@ -0,0 +1,434 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Media;
+
+namespace Quiz
+{
+    public partial class Form2 : Form
+    {
+        private List<Button> ABCDButton = new List<Button>();
+        private int RightAnswer = 0;
+        private int RightChoice_Button = 0;
+
+        private Random random = new Random();
+
+
+        private List<string> Questions = new List<string>(); //忘记改用structure或者元组了，不想改。
+
+        private List<string> Answers = new List<string>(); // when finsh one question, the answer will be deleted with the question.
+
+        private List<string> AnswersWithoutDeleting = new List<string>();
+
+        private List<(string, string)> RecordingWrongAnswer = new List<(string, string)>();
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        //private void RemoveReapted<T>(ref T List_) where T : ICollection<T>{
+        //    ICollection<T> returnList = new typeof(List_);
+        //    foreach (var item in List_)
+        //    {
+
+        //    }
+
+        //}
+        //private void RemoveRepeated<T>(ref ICollection<T> list_)
+        //{
+        //    ICollection<T> returnList = new List<T>();
+        //    bool Included = false;
+        //    foreach (var item in list_)
+        //    {
+        //        foreach (var returnItem in returnList)
+        //        {
+        //            if (item.Equals(returnItem))
+        //            {
+        //                Included = true;
+        //            }
+        //        }
+        //        if (!Included)
+        //        {
+        //            returnList.Add(item);
+        //        }
+        //        else
+        //        {
+        //            Included = false;
+        //        }
+        //    }
+
+        //    list_ = returnList;
+        //}
+        private void RemoveRepeated<T>(ref List<T> list_)
+        {
+            List<T> returnList = new List<T>();
+            bool Included = false;
+            foreach (var item in list_)
+            {
+                foreach (var returnItem in returnList)
+                {
+                    if (item.Equals(returnItem))
+                    {
+                        Included = true;
+                    }
+                }
+                if (!Included)
+                {
+                    returnList.Add(item);
+                }
+                else
+                {
+                    Included = false;
+                }
+            }
+
+            list_ = returnList;
+        }
+        private void InitializeQuiz()
+        {
+
+            try
+            {
+                File.ReadAllText(Form1.quizFile); //先尝试读入一遍，没有问题再继续
+            }
+            catch (Exception ex)
+            {
+                Message.dialog.showError(ex);
+                this.Close();
+                return;
+            }
+
+            string quizAllContent = File.ReadAllText(Form1.quizFile);
+
+            while (quizAllContent != quizAllContent.Replace("\n\n", "\n"))
+            {
+                quizAllContent = quizAllContent.Replace("\n\n", "\n");
+            }
+
+            string[] quizSplitedContent = quizAllContent.Split('\n', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            if (quizSplitedContent.Length == 0)
+            {
+                MessageBox.Show("Could not read any information, please check if the file is wrong or the program has access to it.", "Warning:", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.Close();
+                return;
+            }
+
+            string[] tempSplit = new string[] { "Question", "Answer" };
+            try
+            {
+                foreach (var text in quizSplitedContent)
+                {
+                    tempSplit = text.Split('|');
+                    if (tempSplit.Length == 2)
+                    {
+                        tempSplit[0] = tempSplit[0].Replace("【***[**NenLine**]***】", "\n");
+                        tempSplit[1] = tempSplit[1].Replace("【***[**NenLine**]***】", "\n");
+
+                        Questions.Add(tempSplit[0]);
+                        Answers.Add(tempSplit[1]);
+
+                        AnswersWithoutDeleting.Add(tempSplit[1]);
+                        //answersWithoutDeleting = answers; 出现问题，会跟着减少
+                    }
+
+                }
+
+            }
+            catch
+            {
+                Message.dialog.Damaged();
+                this.Close();
+            }
+
+            //foreach(var e in questions)
+            //{
+            //    Console.WriteLine($"Question:{e}");
+            //}
+
+            //foreach (var e2 in answers)
+            //{
+            //    Console.WriteLine($"Answer:{e2}");
+            //}
+            //Console.WriteLine(answers.Count);
+
+            recording_start();
+            InitializeQuiz_FormInfo();
+        }
+
+        private void InitializeQuiz_FormInfo()
+        {
+            label_tnum.Text = Questions.Count.ToString();
+            label_time_used.Text = "0";
+            label_incorrect.Text = "0";
+        }
+
+        private void WriteWrongQuestionToFile()
+        {
+
+            if (RecordingWrongAnswer.Count == 0)
+            {
+                return;
+            }
+
+            RemoveRepeated<(string, string)>(ref RecordingWrongAnswer);
+            string TempQuizFile = "";
+            //foreach ((string, string) oneQA in RecordingWrongAnswer)
+            //{
+            //    if(AllText == "")
+            //    {
+            //        AllText = oneQA.Item1 + "|" + oneQA.Item2;
+            //    }
+            //    else
+            //    {
+            //        AllText = AllText + "\n" + oneQA.Item1 + "|" + oneQA.Item2;
+            //    }
+            //}
+            //try
+            //{
+            //    Console.WriteLine(Form1.quizFile + $".{DateTime.UtcNow.Ticks.ToString()}.WrongQuesions.txt");
+            //    File.WriteAllText(Form1.quizFile + $".{DateTime.UtcNow.Ticks.ToString()}.WrongQuesions.txt", AllText);
+
+            //}
+            //catch(Exception ex)
+            //{
+            //    Message.dialog.showError(ex);
+            //}
+            TempQuizFile = Form1.quizFile; //***************************--***************************
+            Form1.quizFile = Form1.quizFile + $".{DateTime.UtcNow.Ticks.ToString()}.WrongQuesions.txt";
+            Form4 form4 = new Form4();
+            form4.Visible = false;
+            foreach ((string, string) oneQA in RecordingWrongAnswer)
+            {
+                form4.richTextBox_q.Text = oneQA.Item1;
+                form4.richTextBox_a.Text = oneQA.Item2;
+                form4.button_add_Click(form4.button_add, new EventArgs());
+            }
+            form4.Save();
+
+            Form1.quizFile = TempQuizFile; //***************************--***************************
+
+        }
+        private bool FinishedQuiz() //里面有个找不出来的bug，直接放弃
+        {
+            recording_end();
+            WriteWrongQuestionToFile();
+            Message.dialog.showHit("Congratulation! You finished all of the question!");
+            return false;
+
+        }
+        private void recording_start()
+        {
+            if (!File.Exists(Form1.quizFile + ".utco"))
+            {
+                File.WriteAllText(Form1.quizFile + ".utco", DateTime.UtcNow.ToString());
+            }
+            else
+            {
+                File.WriteAllText(Form1.quizFile + ".utco", File.ReadAllText(Form1.quizFile + ".utco") + "\n" + DateTime.UtcNow); //utco - UTC opened
+            }
+
+        }
+        private void recording_end()
+        {
+            if (!File.Exists(Form1.quizFile + ".time"))
+            {
+                File.WriteAllText(Form1.quizFile + ".time", label_time_used.Text);
+            }
+            else
+            {
+                File.WriteAllText(Form1.quizFile + ".time", File.ReadAllText(Form1.quizFile + ".time") + "\n" + label_time_used.Text);
+            }
+
+            if (!File.Exists(Form1.quizFile + ".wrong"))
+            {
+                File.WriteAllText(Form1.quizFile + ".wrong", label_incorrect.Text);
+            }
+            else
+            {
+                File.WriteAllText(Form1.quizFile + ".wrong", File.ReadAllText(Form1.quizFile + ".wrong") + "\n" + label_incorrect.Text);
+            }
+
+            //File.WriteAllText(Form1.quizFile + ".time", File.ReadAllText(Form1.quizFile + ".time") + "\n" + label_time_used.Text);
+            //File.WriteAllText(Form1.quizFile + ".wrong", File.ReadAllText(Form1.quizFile + ".wrong") + "\n" + label_incorrect.Text);
+        }
+        private void setOneQuestion()
+        {
+            //----------判断是否完成----------
+            if (Questions.Count == 0)
+            {
+                if (FinishedQuiz() == false)
+                {
+                    this.Close();
+                    return;
+                }
+                else
+                {
+                    InitializeQuiz();
+                }
+            }
+
+            //----------判断是否完成----------
+
+            //----------生成正确答案并添加到form----------
+            int SelectedQuestionIndex = random.Next(0, Questions.Count);
+
+            //Console.WriteLine($"SelectedQuestionIndex:{SelectedQuestionIndex};question:{questions[SelectedQuestionIndex]};answer:{answers[SelectedQuestionIndex]}");
+
+            label_question_name.Text = Questions[SelectedQuestionIndex];
+
+
+            RightChoice_Button = random.Next(0, 4); //0,1,2,3,随机选取一个作为正确答案
+
+            //Console.WriteLine($"RightButton:{RightChoice_Button.ToString()}");
+
+            ABCDButton[RightChoice_Button].Text = Answers[SelectedQuestionIndex];
+
+            Console.WriteLine($"RightChoice_Button:{RightChoice_Button};Q:{Questions[SelectedQuestionIndex]};A:{Answers[SelectedQuestionIndex]}");
+
+            RightAnswer = SelectedQuestionIndex;
+
+            //----------生成正确答案并添加到form----------
+
+            //----------生成错误答案----------
+            int randnum = 0;
+            int times = 0;
+
+            foreach (var singleButton in ABCDButton)
+            {
+                //Console.WriteLine(singleButton.Name);
+                randnum = random.Next(0, AnswersWithoutDeleting.Count);
+
+                while (AnswersWithoutDeleting[randnum] == Answers[SelectedQuestionIndex]) //防止出现同样的答案
+                {
+                    times++;
+                    randnum = random.Next(0, AnswersWithoutDeleting.Count);
+                    if (times > 9999) //尝试次数太多
+                    {
+                        break;
+                    }
+                }
+
+                if (singleButton != ABCDButton[RightChoice_Button])
+                {
+                    singleButton.Text = AnswersWithoutDeleting[randnum]; //21个count,从0开始，所以不加1
+                }
+
+
+            }
+            //----------生成错误答案----------
+
+
+            //----------其它form操作----------
+            label_rnum.Text = Questions.Count.ToString();
+            //----------其它form操作----------
+        }
+
+        private void afterAnswered(bool correct)
+        {
+            if (correct == false)
+            {
+                string RightText = ABCDButton[RightChoice_Button].Text;
+
+                label_info.BackColor = Color.Red;
+                label_info.ForeColor = Color.White;
+                label_info.Text = $"You are wrong, the answer is: \n {RightText} \n Please try again!";
+
+                RecordingWrongAnswer.Add((label_question_name.Text, RightText));
+
+                label_incorrect.Text = (int.Parse(label_incorrect.Text) + 1).ToString();
+            }
+            else
+            {
+                label_info.BackColor = Color.White;
+                label_info.ForeColor = Color.Black;
+                label_info.Text = $"Congratulation! You are correct! Time: {DateTime.Now}; Random number: {random.Next(0, int.MaxValue)}"; //UTC Time:{DateTime.UtcNow}
+
+
+                Action playMusicAct = new Action(playMusic);
+                playMusicAct.BeginInvoke(null, null);
+
+
+                Console.WriteLine($"Right Answer:{RightAnswer}");
+                Answers.RemoveAt(RightAnswer);
+                Questions.RemoveAt(RightAnswer);
+
+                RightAnswer = 0;
+
+                setOneQuestion();
+            }
+
+
+        }
+
+        private void playMusic()
+        {
+            Random random_ms = new Random();
+            SoundPlayer soundPlayer = new SoundPlayer();
+            string MusicList = random_ms.Next(1, 2 + 1).ToString(); //包前不包后
+            string MusicDic = $"C:\\ProgramData\\TakeAQuiz\\Music{MusicList}.wav";
+            if (checkBox_bgm.Checked)
+            {
+                soundPlayer.SoundLocation = MusicDic;
+                soundPlayer.Play();
+            }
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+
+            //#if DEBUG
+            //            timer1.Enabled = false;
+            //#endif
+
+
+            ABCDButton.Add(button_A);
+            ABCDButton.Add(button_B);
+            ABCDButton.Add(button_C);
+            ABCDButton.Add(button_D);
+
+            //foreach(var d in ABCDButton)
+            //{
+            //    Console.WriteLine(d.Name);
+            //}
+
+            //Console.WriteLine(ABCDButton.Count);
+
+
+            InitializeQuiz();
+            //for (int i = 0; i < 99; i++)
+            //{
+            //    Console.WriteLine(random.Next(0, answersWithoutDeleting.Count));
+            //}
+            setOneQuestion();
+        }
+
+        private void AnswerSelected_ButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton == ABCDButton[RightChoice_Button])
+            {
+                afterAnswered(true);
+            }
+            else
+            {
+                afterAnswered(false);
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_time_used.Text = (int.Parse(label_time_used.Text) + 1).ToString();
+        }
+    }
+}
